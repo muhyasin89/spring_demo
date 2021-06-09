88e47fdcd998ba29644c5a5532f599e2896316fa
muhyasin89 <muhyasin89@gmail.com>
2021-06-10 06:23:51 +0700

    [JPA_Repository]add student repository

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,23 +1,21 @@
 package com.example.demo.student;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Service
 public class StudentService {
 
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudents(){
-        return List.of(
-            new Student(
-                1L,
-                "test",
-                "test@gmail.com",
-                LocalDate.of(2000, Month.JANUARY, 5),
-                21
-            )
-        );
+        return studentRepository.findAll();
 	}
 }
