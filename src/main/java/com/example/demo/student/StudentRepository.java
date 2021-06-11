8970e03185b0124025943beeb5daf48e8b25a289
muhyasin89 <muhyasin89@gmail.com>
2021-06-12 02:51:29 +0700

    [post_api]add post api

--- src/main/java/com/example/demo/student/StudentRepository.java
+++ src/main/java/com/example/demo/student/StudentRepository.java
@@ -3,6 +3,7 @@ package com.example.demo.student;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
